c0f0660f54b08e406d5bd0192d689540157a863a
Thang Ngo <hoangthang23091993@gmail.com>
2025-09-10 16:05:08 +0700

    update validate querry get rooms

--- src/dto/rooms/check-availability.dto.ts
+++ src/dto/rooms/check-availability.dto.ts
@@ -1,7 +1,8 @@
+import { Type } from 'class-transformer';
 import { IsUUID, IsDateString, IsInt, Min } from 'class-validator';
 export class CheckAvailabilityDto {
   @IsUUID() hotelId: string;
   @IsDateString() checkIn: string;   // YYYY-MM-DD
   @IsDateString() checkOut: string;  // YYYY-MM-DD
-  @IsInt() @Min(1) guests: number;
+  @Type(() => Number) @IsInt() @Min(1) guests: number;
 }
